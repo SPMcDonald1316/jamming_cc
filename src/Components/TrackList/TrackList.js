140982e36040c1aa5290da2ae0549d346363b341
Sean McDonald <spmcdonald1316@gmail.com>
2020-07-16 21:19:29 -0500

    make components dynamic passing data from App to SearchResults to TrackList to Track

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -6,7 +6,13 @@ class TrackList extends React.Component {
   render() {
     return (
       <div className="TrackList">
-        <Track />
+        {
+          this.props.tracks.map(track => {
+            return (
+              <Track key={track.id} track={track}/>
+            )
+          })
+        }
       </div>
     )
   }
