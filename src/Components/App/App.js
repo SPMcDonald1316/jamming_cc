78c932980b5e0fcf29dc896df252ac822e48e26e
Sean McDonald <spmcdonald1316@gmail.com>
2020-07-18 16:07:47 -0500

    create updatePlaylistName method and pass to Playlist component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -26,6 +26,7 @@ class App extends React.Component {
     };
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
+    this.updatePlaylistName = this.updatePlaylistName.bind(this);
   }
 
   addTrack(track) {
@@ -46,6 +47,12 @@ class App extends React.Component {
     })
   }
 
+  updatePlaylistName(name) {
+    this.setState({
+      playlistName: name
+    })
+  }
+
   render() {
     return (
       <div>
@@ -61,6 +68,7 @@ class App extends React.Component {
               playlistName={this.state.playlistName} 
               playlistTracks={this.state.playlistTracks} 
               onRemove={this.removeTrack}
+              onNameChange={this.updatePlaylistName}
             />
           </div>
         </div>
