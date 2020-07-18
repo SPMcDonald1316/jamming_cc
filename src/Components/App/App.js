4ab7dbd92595e2d432ba67eeb1185a387889aefd
Sean McDonald <spmcdonald1316@gmail.com>
2020-07-18 15:58:56 -0500

    create removeTrack method and pass down to components

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -25,17 +25,25 @@ class App extends React.Component {
     }]
     };
     this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
   }
 
   addTrack(track) {
     if (this.state.playlistTracks.find(savedTrack => savedTrack.id === track.id)) {
       return;
     } else {
-      let playList = this.state.playlistTracks.push(track);
+      let playlist = this.state.playlistTracks.push(track);
       this.setState({
-        playlistTracks: playList
+        playlistTracks: playlist
       })
     }
+  }
+
+  removeTrack(track) {
+    let updatedPlaylist = this.state.playlistTracks.filter(savedTrack => savedTrack.id !== track.id)
+    this.setState({
+      playlistTracks: updatedPlaylist
+    })
   }
 
   render() {
@@ -45,8 +53,15 @@ class App extends React.Component {
         <div className="App">
           <SearchBar />
           <div className="App-playlist">
-            <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack}/>
-            <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks}/>
+            <SearchResults 
+              searchResults={this.state.searchResults} 
+              onAdd={this.addTrack}
+            />
+            <Playlist 
+              playlistName={this.state.playlistName} 
+              playlistTracks={this.state.playlistTracks} 
+              onRemove={this.removeTrack}
+            />
           </div>
         </div>
       </div>
